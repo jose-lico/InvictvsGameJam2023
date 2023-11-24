7d1298e50874346418690e6b79d9ac10bd44fa50
Eurico Martins <euricomartinss@gmail.com>
2023-11-24 23:28:00 +0000

    Genisys config

---
+++ src/genisys/genisys_center.gd
@@ -0,0 +1,280 @@
+extends Node
+class_name GenisysCenter
+
+
+const SERVICE_PREFIX: String = "service::";
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Values
+var protocol_version: String = "v1"
+var use_wss: bool = false;
+var server_address: String = "localhost";
+var server_port: String = "3333";
+var led_pattern_json_path: String = "";
+var blink_pattern_json_path: String = "";
+
+
+var client_websocket: WebSocketPeer = null;
+var server_url: String;
+var skt_is_connected: bool = false;
+var should_ignore_requests: bool = false;
+
+var _id_binds: Dictionary;
+# Led pattern dictionary
+var _ledpatterns_data: Dictionary;
+var _blinkpatterns_data: Dictionary;
+
+# ==================== ====================
+# Godot Override
+# ==================== ====================
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+func _boot_controller():
+	_id_binds =  {all={},input={}};
+
+	set_process(false);
+	_generate_url();
+
+	if(!led_pattern_json_path.is_empty()):
+		_ledpatterns_data = load_json_data(led_pattern_json_path);
+
+	if(!blink_pattern_json_path.is_empty()):
+		_blinkpatterns_data = load_json_data(blink_pattern_json_path);
+
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+func _ready():
+		client_websocket = WebSocketPeer.new();
+		# client_websocket.set_supported_protocols(["lws-mirror-protocol "]);
+		bind_id_to_callback("hardware/inputs/status_change", Callable(self, "_on_input_change"));
+		register();
+		_connect_socket();
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+func _process(_delta):
+	if( client_websocket != null):
+		client_websocket.poll();
+
+		var socket_State = client_websocket.get_ready_state();
+		match socket_State:
+			# Connecting to
+			WebSocketPeer.STATE_CONNECTING:
+				print("Connecting to server...");
+
+			# During a connected socket state
+			WebSocketPeer.STATE_OPEN:
+				if(!self.skt_is_connected):
+					_on_connection_established();
+				else:
+					while (client_websocket.get_available_packet_count()):
+						_on_data_received(client_websocket.get_packet());
+
+			# Closing connection
+			WebSocketPeer.STATE_CLOSING:
+				print("Closing connection...");
+
+
+			WebSocketPeer.STATE_CLOSED:
+				_on_server_close_request(client_websocket.get_close_code(), client_websocket.get_close_reason());
+				set_process(false);
+
+# ==================== ====================
+# Public
+# ==================== ====================
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Binds a response id to a callback function
+func bind_id_to_callback(id: String, callback: Callable):
+	if(_id_binds.all.has(id)):
+		_id_binds.all[id].append(callback);
+	else:
+		_id_binds.all[id] = [callback];
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+func bind_input_to_callback(id:String, callback: Callable):
+	if(_id_binds.input.has(id)):
+		_id_binds.input[id].append(callback);
+	else:
+		_id_binds.input[id] = [callback];
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# 
+func send_data(id: String, payload: Dictionary = {}):
+	if(skt_is_connected):
+		var packed: Dictionary = {"id": "%s/%s" % [protocol_version, id] };
+		packed.merge(payload, true);
+		
+		print("Sending data id: [%s]" % id);
+
+		var error = client_websocket.send(JSON.stringify(packed).to_utf8_buffer());
+		if(error != OK):
+			print_debug("Failed to send package. Code [%s]" % error);
+
+
+
+# ==================== ====================
+# Protected
+# ==================== ====================
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Override this function
+# Called before connect to the socket
+func register():
+	print_debug("No Implementation Exception");
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Override this function
+# Called after connection established
+func connection_established():
+	print_debug("No Implementation Exception");
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Override this function
+# Called before exiting the tree
+func on_destroy():
+	print_debug("No Implementation Exception");
+
+
+# ==================== ====================
+# Private
+# ==================== ====================
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Creates the connection url
+func _generate_url():
+	server_url = "ws";
+	if(use_wss):
+		server_url += "s";
+	server_url+= "://%s:%s" % [server_address, server_port];
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Loads and parses a json file by path
+func load_json_data(path)-> Dictionary:
+	var data: Dictionary = Dictionary();
+	if(FileAccess.file_exists(path)):
+		var file = FileAccess.open(path, FileAccess.READ);
+		var str_data: String = file.get_as_text();
+
+		var json_file: JSON = JSON.new();
+		var er = json_file.parse(str_data);
+		if(er == OK):
+			data = json_file.data;
+	
+	return data;
+
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Creates the websocket signal connections and process the connect to url 
+func _connect_socket():
+	#warning-ignore-all:return_value_discarded
+	
+	# if(!client_websocket.is_connected("connection_closed", Callable(self, "_on_closed"))):
+	# 	client_websocket.connect("connection_closed", Callable(self, "_on_closed"));
+
+	# if(!client_websocket.is_connected("server_close_request", Callable(self, "_on_server_close_request"))):
+	# 	client_websocket.connect("server_close_request", Callable(self, "_on_server_close_request"));
+
+	# if(!client_websocket.is_connected("connection_error", Callable(self, "_on_connection_error"))):
+	# 	client_websocket.connect("connection_error", Callable(self, "_on_connection_error"));
+
+	# if(!client_websocket.is_connected("connection_established", Callable(self, "_on_connection_established"))):
+	# 	client_websocket.connect("connection_established", Callable(self, "_on_connection_established"));
+
+	# if(!client_websocket.is_connected("data_received", Callable(self, "_on_data_received"))):
+	# 	client_websocket.connect("data_received", Callable(self, "_on_data_received"));
+
+	var error = client_websocket.connect_to_url(server_url);
+	if(error != OK):
+		print_debug("Failed to connect with error %s" % error);
+		return;
+	
+	# is_connected = true;
+	set_process(true);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Disconnect the connected socket
+func _disconnect_socket():
+	if(skt_is_connected):
+		client_websocket.disconnect_from_host(1002, "closing game");
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Emitted when the connection to the server is closed.
+# was_clean_close will be true if the connection was shutdown cleanly.
+func _on_closed(was_clean_close: bool):
+	print("Connection closed. [Clear: %s]" % was_clean_close);
+	
+	skt_is_connected = false;
+	set_process(skt_is_connected);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Emitted when the server requests a clean close. 
+# You should keep polling until you get a connection_closed signal to achieve the clean close. 
+func _on_server_close_request(code: int, reason: String):
+	print("Server requested a close connection with code [%s]\n%s" % [str(code), reason]);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Emitted when the connection to the server fails.
+func _on_connection_error():
+	print("An error occurred, continuing...");
+
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Emitted when a connection with the server is established, protocol will contain the sub-protocol agreed with the server.
+func _on_connection_established():
+	print("Connected to [%s:%s]" %  [server_address, server_port]);
+	skt_is_connected = true;
+	connection_established();
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Emitted when a WebSocket message is received.
+func _on_data_received(data: PackedByteArray ):
+	var parsed_data: Dictionary = JSON.parse_string(data.get_string_from_utf8());
+	# print(JSON.stringify(parsed_data, "\t"));
+
+	if(parsed_data.has("id")):
+		var data_id: String = parsed_data.id;
+		if(SERVICE_PREFIX in data_id):
+			data_id = data_id.substr(SERVICE_PREFIX.length());
+			_dispatch_response(data_id, parsed_data.payload);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Calls the subscription callback function with the recieved payload
+func _dispatch_response(id: String, payload: Dictionary):
+	if(_id_binds.all.has(id)):
+		for callback in _id_binds.all[id]:
+			(callback as Callable).call(payload);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Callback for a input status_change response
+func _on_input_change(payload:Dictionary):
+	if(payload.has("data") && payload.data.has("name")):
+		if(_id_binds.input.has(payload.data.name)):
+			for callback in _id_binds.input[payload.data.name]:
+				(callback as Callable).call(payload);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+func _notification(what):
+	match what:
+		NOTIFICATION_WM_CLOSE_REQUEST:
+			on_destroy();
+			call_deferred("_disconnect_socket");
---
+++ src/genisys/genisys_controller.gd
@@ -0,0 +1,170 @@
+extends GenisysCenter
+
+
+# ==================== ====================
+# Public
+# ==================== ====================
+
+func _enter_tree():
+	server_address = "192.168.220.109"
+	_boot_controller();
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Override this function
+# Called before exiting the tree
+# func on_destroy():
+# 	if(is_connected):
+# 		send_data("core/settings/remove_capability", {payload="hardware"});
+# 		send_data("hardware/jackpot_display/set_text", {payload={"alignment": "right", "text": "00"}});
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Override this function
+# Called before connect to the socket
+func register():
+	bind_input_to_callback("play", Callable(self, "_on_play_state_change"))
+	bind_input_to_callback("bet_more", Callable(self, "_on_bet_more_state_change"));
+	bind_input_to_callback("autoplay", Callable(self, "_on_auto_state_change"));
+	bind_id_to_callback("hardware/led_strips/get_info", Callable(self, "_on_led_strips_get_info"));
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Override this function
+# Called after connection established
+func connection_established():
+	pass
+	# send_data("core/settings/remove_capability", {payload="hardware"});
+	# send_data("core/settings/add_capability", {payload="hardware"});
+
+	# send_data("core/settings/update_configuration", {payload={
+	# 	"hardware/cabinet_type":2, "hardware/io_type": "quixant", "hardware/io_light_tower_enable": false}});
+	
+	# send_data("core/settings/get_configuration");
+	# send_data("hardware/led_strips/get_info");
+
+	# if(!_ledpatterns_data.is_empty()):
+	# 	send_data("hardware/led_strips/add_patterns", {payload=_ledpatterns_data.patterns});
+	# 	send_data("hardware/led_strips/set_pattern", {payload="first_example"});
+
+	# if(!_blinkpatterns_data.is_empty()):
+	# 	send_data("hardware/outputs/add_blink_patterns", {payload=_blinkpatterns_data.patterns});
+	
+
+# ==================== ====================
+# Private
+# ==================== ====================
+
+# - - - - - - - - - - - - - - - - - - - - -
+# func _on_get_configuration(payload: Dictionary):
+# 	print(JSON.print(payload, "\t"));
+
+func _on_led_strips_get_info(payload: Dictionary):
+	print("_on_led_strips_get_info callback");
+	print(JSON.stringify(payload, "\t"));
+
+
+# - - - - - - - - - - - - - - -
+# Callback on enter state
+func _on_enter_state(_current_state):
+	if(_current_state == null):
+		return;
+	
+
+	match _current_state:
+		"idle", "end_game":
+			send_data("hardware/outputs/stop_blink_pattern", {payload={group="draw_group"}});
+			send_data("hardware/outputs/start_blink_pattern", {payload={"group": "idle_group", "id": "test_pattern"}});
+
+
+		"draw_balls":
+			send_data("hardware/outputs/stop_blink_pattern", {payload={group="idle_group"}});
+			send_data("hardware/outputs/start_blink_pattern", {payload={"group": "draw_group", "id": "test_pattern"}});
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Callback funtion to check the availability of jackpot
+# func _on_jackpot_data_set(jackpot_data):
+# 	if(jackpot_data.size() == 0):
+# 		return;
+	
+# 	ReduxCenter.unsubscribe(self, "_on_jackpot_data_set");
+# 	_rdx_jackpot_data = jackpot_data;
+# 	_jackpot_target_id = jackpot_data.keys().front();
+
+# 	ReduxCenter.subscribe(self, "_on_jackpot_state_change",
+# 		"gamelogic.jackpot.jackpot_state.%s" % _jackpot_target_id, self);
+
+
+# # - - - - - - - - - - - - - - - - - - - - -
+# # Callback function for the redux subscription jackpot_state
+# func _on_jackpot_state_change(jackpot_state):
+# 	send_data("hardware/jackpot_display/set_text", {payload={
+# 		"alignment": "right",
+# 		"text": jp_value.insert(jp_value.length() - 2, '.')
+# 	}});
+
+
+# - - - - - - - - - - - - - - -
+# # On Jackpot start callback event
+# func _on_feature_jackpot_start(_evt=null):
+# 	send_data("hardware/outputs/stop_blink_pattern", {payload={group="draw_group"}});
+# 	send_data("hardware/outputs/start_blink_pattern", {payload={"group": "jackpot_group", "id": "test_pattern"}});
+
+
+# - - - - - - - - - - - - - - -
+# On Jackpot end callback event
+# func _on_feature_jackpot_end(_evt=null):
+# 	send_data("hardware/outputs/stop_blink_pattern", {payload={group="jackpot_group"}});
+# 	send_data("hardware/outputs/start_blink_pattern", {payload={"group": "draw_group", "id": "test_pattern"}});
+
+
+# # - - - - - - - - - - - - - - - - - - - - -
+# func _on_animate_last_prizes(_evt=null):
+# 	send_data("hardware/led_strips/set_pattern", {payload="second_example"});
+
+# 	send_data("hardware/outputs/stop_blink_pattern", {payload={group="idle_group"}});
+# 	send_data("hardware/outputs/start_blink_pattern", {payload={
+# 		"group": "draw_group", "id": "test_pattern" }});
+
+
+# # - - - - - - - - - - - - - - - - - - - - -
+# func _on_prize_animation_completed(_evt=null):
+# 	send_data("hardware/led_strips/set_pattern", {payload="first_example"});
+
+# 	match (_rdx_gamelogic.state.current):
+# 		"draw_balls":
+# 			send_data("hardware/outputs/start_blink_pattern", {payload={"group": "idle_group",	"id": "test_pattern" }});
+
+# 		_:
+# 			send_data("hardware/outputs/stop_blink_pattern", {payload={group="draw_group"}});
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Callback function for the play button
+func _on_play_state_change(payload: Dictionary):
+	# print("on play pressed");
+	print(JSON.stringify(payload, "\t"));
+# 	if(payload.data.input_state == "active"):
+# 		EventCenter.emit("evt_key_space", self);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Callback function for the betmore button
+# func _on_bet_more_state_change(payload: Dictionary):
+# 	if(payload.data.input_state == "active"):
+# 		if(["end_game", "idle"].has(_rdx_gamelogic.state.current)):
+# 			EventCenter.emit("evt_btn_increase_bet_level", self);
+
+
+# - - - - - - - - - - - - - - - - - - - - -
+# Callback function for the autoplay button
+# func _on_auto_state_change(payload: Dictionary):
+# 	if(payload.data.input_state == "active"):
+# 		var current_panel_state = _rdx_gamelogic.menus.stacked.values().back();
+# 		if ([StackedMenuController.State.OPEN].has(current_panel_state.state)):
+# 			EventCenter.emit("evt_btn_stacked_menu_trigger", self,
+# 				{"menu": current_panel_state.panel, "state": StackedMenuController.State.REQUEST_CLOSE});
+# 		elif([StackedMenuController.State.CLOSED].has(current_panel_state.state)):
+# 			EventCenter.emit("evt_btn_stacked_menu_trigger", self,
+# 				{"menu": current_panel_state.panel, "state": StackedMenuController.State.REQUEST_OPEN});

14124d8fe9280ef09ae9bdfbc9c7b41efc39fd70
jose-lico <zelico98@gmail.com>
2023-11-25 01:51:44 +0000

    perf

--- src/fps_counter.gd
+++ src/fps_counter.gd
@@ -1,4 +1,18 @@
 extends Label
 
-func _process(delta):
-	text = "FPS:" + str(Engine.get_frames_per_second());
+@onready var viewport1 = get_node("../Cameras/SubViewport")
+@onready var viewport2 = get_node("../Cameras/SubViewport")
+
+func _ready():
+	RenderingServer.viewport_set_measure_render_time(viewport1.get_viewport_rid(), true)
+	RenderingServer.viewport_set_measure_render_time(viewport2.get_viewport_rid(), true)
+
+func _process(_delta):
+	var cpu_time = RenderingServer.viewport_get_measured_render_time_cpu(viewport1.get_viewport_rid()) +\
+		RenderingServer.viewport_get_measured_render_time_cpu(viewport2.get_viewport_rid()) +\
+		RenderingServer.get_frame_setup_time_cpu();
+		
+	var gpu_time = RenderingServer.viewport_get_measured_render_time_gpu(viewport1.get_viewport_rid()) +\
+		RenderingServer.viewport_get_measured_render_time_gpu(viewport2.get_viewport_rid());
+	
+	text = "FPS:" + str(Engine.get_frames_per_second()) + " | CPU:" + "%.3f"%cpu_time + "ms | GPU:" + "%.3f"%gpu_time + "ms | Total:" + "%.3f"%(cpu_time + gpu_time) + "ms";

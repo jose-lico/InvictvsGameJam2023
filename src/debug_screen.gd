91bdb503683380ef370755fee2e8d0e6a1d971c5
jose-lico <zelico98@gmail.com>
2023-11-25 16:58:52 +0000

    saloon changes

--- src/debug_screen.gd
+++ src/debug_screen.gd
@@ -1,13 +1,5 @@
 extends Label
 
-@onready var viewport1 = get_node("../Cameras/SubViewport")
-@onready var viewport2 = get_node("../Cameras/SubViewport")
-
-func _ready():
-	RenderingServer.viewport_set_measure_render_time(viewport1.get_viewport_rid(), true)
-	RenderingServer.viewport_set_measure_render_time(viewport2.get_viewport_rid(), true)
-	Genisys.sig_on_debug.connect(add_debug_text)
-
 func add_debug_text(data):
 	if(data.payload.has("data") && !(data.payload.data is String)):
 		if (typeof(data.payload.data) != TYPE_DICTIONARY  ):

703d48b3b4d60f91ca97b444c96706464e39aae5
jose-lico <zelico98@gmail.com>
2023-11-25 02:15:19 +0000

    update genisys script

--- app.genisys.lua
+++ app.genisys.lua
@@ -3,10 +3,10 @@ local genisys = require("genisys")
 local game = genisys.create_application("game")
 
 game.name = "Invictvs Game Jam 2023"
-game.version = "0.0.1"
+game.version = "0.0.69"
 game.description = [[Our awesome game.]]
 
 local exe = game:create_process()
-exe.command = { genisys.get_path("Invictvs Game Jam 2023.x86_64") }
+exe.command = { genisys.get_path("Invictvs Game Jam 2023.x86_64"), "--rendering-driver", "opengl3" }
 
 return game
